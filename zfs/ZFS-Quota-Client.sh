5e3ac5f3516ddaacb2384ed10be54c6aefce4c79
Adam Boutcher <adam.j.boutcher@durham.ac.uk>
2017-06-29 11:52:57 +0100

    Tweaked the output so it stays inline better, added some error surpesssion

--- zfs/ZFS-Quota-Client.sh
+++ zfs/ZFS-Quota-Client.sh
@@ -3,22 +3,21 @@
 
 servers[0]="/mt/home"
 
-echo "";
-echo -e " Quota Report for $USER"
-echo -e " Mount\t\t\t\tUsed\t\tTotal\t\tLast Checked"
+printf "\n Quota Report for $USER\n"
+printf ' %-30s %-15s %-15s %-25s\n' "Mount Point" "Used" "Total" "Last Checked";
 
 for i in "${servers[@]}"; do
-	zquota=$(cat $i/quota.zfs | grep $USER);
+	zquota=$(cat $i/quota.zfs 2>/dev/null | grep $USER);
 	if [[ ! -z "$zquota" ]]; then
 		zused=$(echo $zquota | awk -F'::' '{print $2}' | numfmt --to=iec);
 		ztotal=$(echo $zquota | awk -F'::' '{print $3}');
 		if [[ $ztotal -ne "none" ]]; then 
 			ztotal=$(echo $ztotal | numfmt --to=iec);
 		fi
-		zuperc=$(echo $zquota | awk -F'::' '{print $4}');
+		zperc=$(echo $zquota | awk -F'::' '{print $4}');
 		zage=$(date +"%c" -d @$(stat -c %Z $i/quota.zfs))
-		echo -e " $i\t\t\t$zused ($zuperc)\t$ztotal\t\t$zage"
+		printf ' %-30s %-15s %-15s %-25s\n' "$i" "$zused ($zperc)" "$ztotal" "$zage";
 	fi
 done;
-echo "";
+printf "\n"
 exit 0;
