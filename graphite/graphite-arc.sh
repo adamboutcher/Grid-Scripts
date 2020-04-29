071319fefc4a6ce3d8c44f0e1350d37f722741dc
Adam Boutcher <adam.j.boutcher@durham.ac.uk>
2020-04-29 13:16:39 +0100

    Added ControlDir Percent Used and fixed some other minor issues

--- graphite/graphite-arc.sh
+++ graphite/graphite-arc.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#/usr/local/sbin/graphite-arc.sh
+#
 # Nordugrid ARC Graphite Metrics - Similar to GanliARC (nordugrid-arc-gangliarc).
 # This is by no means fully featured and is very poorly tested.
 # 2020 - Adam Boutcher - Durham University (UKI-SCOTGRID-DURHAM).
+#
 
 GRAPHITE="172.16.0.1"
 GPORT="2003"
@@ -32,8 +35,8 @@ check_bin df
 
 # Get ARC Variables
 JOBLOG=$(cat /etc/arc.conf | grep "joblog" | awk -F "=" '{print $2}' | sed 's/\"//g');
-CONTROLDIR=$(cat /etc/arc.conf | grep "controldir" | awk -F "=" '{print $2}' | sed 's/\"//g');
-WORKDIR=$(cat /etc/arc.conf | grep "sessiondir" | awk -F "=" '{print $2}' | sed 's/\"//g');
+CONTROLDIR=$(cat /etc/arc.conf | grep "controldir" | grep -v " drain" | awk -F "=" '{print $2}' | sed 's/\"//g');
+WORKDIR=$(cat /etc/arc.conf | grep "sessiondir" | grep -v " drain" | awk -F "=" '{print $2}' | sed 's/\"//g');
 
 # Reading a Log
 LRMS_COUNT=$(cat $JOBLOG | grep "LRMS error" | grep $(date '+%Y-%m-%d' -d "-1 Minute") | grep $(date +%H:%M -d "-1 Minute") | wc -l);
@@ -43,7 +46,7 @@ FAIL_COUNT=$(cat $JOBLOG | grep -i "f" | grep -i "fail" | grep $(date '+%Y-%m-%d
 HEARTBEAT=$(echo $(date +%s)-$(stat -c %Y $CONTROLDIR/info.xml) | bc)
 
 # Counting directories
-PROCESSING=$(echo $(ls $CONTROLDIR/processing/*.status 2>/dev/null | wc -l)-2 | bc)
+PROCESSING=$(ls $CONTROLDIR/processing/*.status 2>/dev/null | wc -l)
 ACCEPTED=$(grep "ACCEPTED" $CONTROLDIR/*/*.status | wc -l)
 PREPARING=$(grep "PREPARING" $CONTROLDIR/*/*.status | wc -l)
 SUBMIT=$(grep "SUBMIT" $CONTROLDIR/*/*.status | wc -l)
@@ -57,7 +60,7 @@ TOTAL=$(ls $CONTROLDIR/*/*.status | wc -l)
 # Grabbing FS Info
 CONTROLDIR_USED=$(df $CONTROLDIR | tail -n1 | awk '{print $3}')
 CONTROLDIR_AVAIL=$(df $CONTROLDIR | tail -n1 | awk '{print $4}')
-CONTROLDIR_PERC=$(df $CONTROLDIR | tail -n1 | awk '{print $5}' | sed 's/\%//g') 
+CONTROLDIR_PERC=$(df $CONTROLDIR | tail -n1 | awk '{print $5}' | sed 's/\%//g')
 WORKDIR_USED=$(df $WORKDIR | tail -n1 | awk '{print $3}')
 WORKDIR_AVAIL=$(df $WORKDIR | tail -n1 | awk '{print $4}')
 WORKDIR_PERC=$(df $WORKDIR | tail -n1 | awk '{print $5}' | sed 's/\%//g')
